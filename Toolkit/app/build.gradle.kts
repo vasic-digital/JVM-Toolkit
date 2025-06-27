89a3895042a25230a86f93523af10008981f0318
Milos Vasic <i@mvasic.ru>
2025-06-27 21:03:12 +0300

    Init.

---
+++ Toolkit/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+
+    id("buildsrc.convention.kotlin-jvm")
+
+    application
+}
+
+dependencies {
+
+
+}
+
+application {
+
+    mainClass = "digital.vasic.app.AppKt"
+}
---
+++ Toolkit/settings.gradle.kts
@@ -0,0 +1,17 @@
+dependencyResolutionManagement {
+
+    @Suppress("UnstableApiUsage")
+    repositories {
+
+        mavenCentral()
+    }
+}
+
+plugins {
+
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+include(":app")
+
+rootProject.name = "Toolkit"
