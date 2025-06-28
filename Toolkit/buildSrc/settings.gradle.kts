924599ca2743255a6e406b21117cfec6b4117c67
Milos Vasic <i@mvasic.ru>
2025-06-28 20:53:10 +0300

    Repositories added.

--- Toolkit/buildSrc/settings.gradle.kts
+++ Toolkit/buildSrc/settings.gradle.kts
@@ -1,14 +1,26 @@
 dependencyResolutionManagement {
 
-    // Use Maven Central and the Gradle Plugin Portal for resolving dependencies in the shared build logic (`buildSrc`) project.
     @Suppress("UnstableApiUsage")
     repositories {
+
+        mavenLocal()
+
+        maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/jetbrains") }
+        maven { url = uri("https://mirrors.ustc.edu.cn/jetbrains/") }
+        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/jetbrains/") }
+        maven { url = uri("https://jitpack.io") }
+
         mavenCentral()
+        google()
     }
 
-    // Reuse the version catalog from the main build.
     versionCatalogs {
+
         create("libs") {
+
             from(files("../gradle/libs.versions.toml"))
         }
     }
