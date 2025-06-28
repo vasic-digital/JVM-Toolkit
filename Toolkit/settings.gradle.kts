03b711923ad75f3ae03febd80327a32ec026ea6f
Milos Vasic <i@mvasic.ru>
2025-06-28 20:22:24 +0300

    Repositories added.

--- Toolkit/settings.gradle.kts
+++ Toolkit/settings.gradle.kts
@@ -3,7 +3,18 @@ dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
 
+        mavenLocal()
+
+        maven { url = uri("https://repo.huaweicloud.com/repository/maven/") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/jetbrains") }
+        maven { url = uri("https://mirrors.ustc.edu.cn/jetbrains/") }
+        maven { url = uri("https://mirrors.tuna.tsinghua.edu.cn/jetbrains/") }
+        maven { url = uri("https://jitpack.io") }
+
         mavenCentral()
+        google()
     }
 }
 
